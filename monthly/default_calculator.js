0dbdd8c37673f45794c044812f841b9ac9db6db3
Evheniq <qundola@gmail.com>
2023-01-26 23:26:35 +0200

    Create calc factory

--- monthly/default_calculator.js
+++ monthly/default_calculator.js
@@ -6,45 +6,60 @@ const data = await JSON.parse(
     textJson.toString()
 );
 
-class DefaultCalculator{
-    constructor(data, compareDateItems) {
-        this.data = data
+class GeoParamsCalculatorFactory{
+    create(type, data){
+        if(type === "annual daily"){
+            return new GeoParamsCalculator(data, [0, 1]);
+        }
+    }
+}
 
-        this.compareDateItems = compareDateItems
-        this.result = {}
+class GeoParamsCalculator{
+    constructor(data, compareDateItems) {
+        this.data = data;
+
+        this.compareDateItems = compareDateItems;
+        this.result = {};
     }
 
     calc(selector){
         for (const item of this.data){
-            const splitedDateSelector = item.date.split(".")
+            const splitDateSelector = item.date.split(".");
             let currentDateSelector = [];
             for (const compareItem of this.compareDateItems){
-                currentDateSelector.push(splitedDateSelector[compareItem])
+                currentDateSelector.push(splitDateSelector[compareItem])
             }
 
-            const currentDateSelectorString = currentDateSelector.join(".")
+            const currentDateSelectorString = currentDateSelector.join(".");
 
             if (this.result[currentDateSelectorString]) {
-                this.result[currentDateSelectorString]["count"] = this.result[currentDateSelectorString]["count"] + 1;
-                this.result[currentDateSelectorString]["sum"] = this.result[currentDateSelectorString]["sum"] + item.data[selector];
-                this.result[currentDateSelectorString]["result"] = this.result[currentDateSelectorString]["count"] / this.result[currentDateSelectorString]["sum"];
+                const currentResultSlot = this.result[currentDateSelectorString];
+                currentResultSlot["count"] = currentResultSlot["count"] + 1;
+                currentResultSlot["sum"] = currentResultSlot["sum"] + item.data[selector];
+                currentResultSlot["result"] = currentResultSlot["count"] / currentResultSlot["sum"];
             } else {
-                this.result[currentDateSelectorString] = {}
-                this.result[currentDateSelectorString]["count"] = 1;
-                this.result[currentDateSelectorString]["sum"] = item.data[selector];
-                this.result[currentDateSelectorString]["result"] = item.data[selector];
+                this.result[currentDateSelectorString] = {};
+                const currentResultSlot = this.result[currentDateSelectorString];
+                currentResultSlot["count"] = 1;
+                currentResultSlot["sum"] = item.data[selector];
+                currentResultSlot["result"] = item.data[selector];
             }
         }
+
+        // this.saveData(this.result, "")
     }
 
-    async saveExtractedData(path){
-        let fileHandle = await open(, 'w');
+    async saveData(data, path){
+        let fileHandle = await open(path, 'w');
         await fileHandle.writeFile(JSON.stringify(this.result));
         console.log("Json saved!")
     }
 }
 
-const monthly = new DefaultCalculator(data, [1, 2])
+// const monthly = new GeoParamsCalculator(data, [1, 2])
+const calcFactory = new GeoParamsCalculatorFactory()
+const monthly = calcFactory.create("annual daily", data)
+
 monthly.calc("air_temperature")
 console.log(
     monthly.result
